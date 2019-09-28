a4d78d0d6ebaeb0a34fcb788703b7e6a60bcb3bb
leandrocodes <leaxviana140@gmail.com>
2019-09-28 16:52:08 -0400

    header logo

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,6 +1,12 @@
 <template>
-  <header class="header container-fluid">
-    ...
+  <header class="header">
+    <b-nav align="left">
+      <b-nav-item>
+        <nuxt-link to="/">
+          <img class="header-logo" src="../assets/imgs/logomarca-taprocurando.svg">
+        </nuxt-link>
+      </b-nav-item>
+    </b-nav>
   </header>
 </template>
 <script>
@@ -10,5 +16,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
 </style>
