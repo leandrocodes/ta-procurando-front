c853c171e84cd9dd28af1a7363ea485eb527940f
leandrocodes <leaxviana140@gmail.com>
2019-09-28 00:23:27 -0400

    starting the navbar

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,6 +1,106 @@
 <template>
   <header class="header container-fluid">
-    leandro viana
+    <div class="global-nav-principal">
+      <a href="index.html" class="logo-link w-inline-block w--current">
+        <img src="../assets/imgs/logomarca-taprocurando.svg" width="229" alt class="logomarca-traprocurando" />
+      </a>
+      <div data-w-id="a4d5a11e-1436-c632-cc44-d09a4c6eed9d" class="burger-button w-hidden-main w-hidden-medium">
+        <div data-w-id="a4d5a11e-1436-c632-cc44-d09a4c6eed9e" class="burger-line"></div>
+        <div data-w-id="a4d5a11e-1436-c632-cc44-d09a4c6eed9f" class="burger-line"></div>
+        <div data-w-id="a4d5a11e-1436-c632-cc44-d09a4c6eeda0" class="burger-line"></div>
+      </div>
+      <div class="nav-buttons-wrapper w-hidden-small w-hidden-tiny">
+        <a href="cadastroseuimovel.html" data-w-id="a4d5a11e-1436-c632-cc44-d09a4c6eeda2" class="global-nav-button w-inline-block">
+          <div>anuncie seu imóvel</div>
+          <div class="left-line"></div>
+          <div class="right-line"></div>
+          <div class="bottom-line"></div>
+          <div class="top-line"></div>
+        </a>
+        <a href="#" data-w-id="a4d5a11e-1436-c632-cc44-d09a4c6eedae" class="global-nav-button w-inline-block">
+          <div>viajante</div>
+          <div class="left-line"></div>
+          <div class="right-line"></div>
+          <div class="bottom-line"></div>
+          <div class="top-line"></div>
+        </a>
+        <a href="#" data-w-id="a4d5a11e-1436-c632-cc44-d09a4c6eedb5" class="global-nav-button w-inline-block">
+          <div>Anunciante</div>
+          <div class="left-line"></div>
+          <div class="right-line"></div>
+          <div class="bottom-line"></div>
+          <div class="top-line"></div>
+        </a>
+        <a href="fale-conosco.html" data-w-id="a4d5a11e-1436-c632-cc44-d09a4c6eedbc" class="global-nav-button w-inline-block">
+          <div>contato</div>
+          <div class="left-line"></div>
+          <div class="right-line"></div>
+          <div class="bottom-line"></div>
+          <div class="top-line"></div>
+        </a>
+      </div>
+      <div data-w-id="a4d5a11e-1436-c632-cc44-d09a4c6eedc3" class="mobile-nav w-hidden-main w-hidden-medium">
+        <div class="nav-buttons-wrapper">
+          <a href="#" data-w-id="a4d5a11e-1436-c632-cc44-d09a4c6eedc5" class="global-nav-button w-inline-block">
+            <div>Anunciar</div>
+            <div class="left-line"></div>
+            <div class="right-line"></div>
+            <div class="bottom-line"></div>
+            <div class="top-line"></div>
+          </a>
+          <a href="#" data-w-id="a4d5a11e-1436-c632-cc44-d09a4c6eedcc" class="global-nav-button w-inline-block">
+            <div>Viajante</div>
+            <div class="left-line"></div>
+            <div class="right-line"></div>
+            <div class="bottom-line"></div>
+            <div class="top-line"></div>
+          </a>
+          <a href="#" data-w-id="a4d5a11e-1436-c632-cc44-d09a4c6eedd3" class="global-nav-button w-inline-block">
+            <div>Anunciante</div>
+            <div class="left-line"></div>
+            <div class="right-line"></div>
+            <div class="bottom-line"></div>
+            <div class="top-line"></div>
+          </a>
+          <a href="#" data-w-id="a4d5a11e-1436-c632-cc44-d09a4c6eedda" class="global-nav-button w-inline-block">
+            <div>Contato</div>
+            <div class="left-line"></div>
+            <div class="right-line"></div>
+            <div class="bottom-line"></div>
+            <div class="top-line"></div>
+          </a>
+        </div>
+      </div>
+      <!--  Menu Mobile Principal -->
+
+      <!--  Linguas -->
+      <div class="languages">
+        <div data-w-id="3482c127-44d8-c6fe-6674-0c1153356127" class="links-wrapper-2">
+          <a href="#" class="lang w-inline-block">
+            <img src="images/bandeira-brasil.png" alt class="icone-linguas" />
+          </a>
+          <a href="#" class="lang w-inline-block">
+            <img src="images/bandeira-brasil.png" alt class="icone-linguas" />
+          </a>
+          <a href="#" class="lang w-inline-block">
+            <img src="images/bandeira-brasil.png" alt class="icone-linguas" />
+          </a>
+          <a href="#" class="lang w-inline-block">
+            <img src="images/bandeira-brasil.png" alt class="icone-linguas" />
+          </a>
+          <a href="#" class="lang w-inline-block">
+            <img src="images/bandeira-brasil.png" alt class="icone-linguas" />
+          </a>
+          <a href="#" class="lang w-inline-block">
+            <img src="images/bandeira-brasil.png" alt class="icone-linguas" />
+          </a>
+        </div>
+        <div class="trigger-nav">
+          <img src="images/arrow-down.svg" data-w-id="3482c127-44d8-c6fe-6674-0c1153356144" alt class="arrow" />
+        </div>
+      </div>
+      <!--  Linguas -->
+    </div>
   </header>
 </template>
 <script>
@@ -9,6 +109,26 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.logomarca-taprocurando.w--current {
+  position: absolute;
+  left: 0%;
+  top: 0%;
+  right: auto;
+  bottom: auto;
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  margin-top: 10px;
+  margin-left: -118px;
+  -webkit-box-pack: start;
+  -webkit-justify-content: flex-start;
+  -ms-flex-pack: start;
+  justify-content: flex-start;
+  -webkit-box-align: center;
+  -webkit-align-items: center;
+  -ms-flex-align: center;
+  align-items: center;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container-fluid">
+  <div class="index">
     <AppHeader />
   </div>
 </template>
