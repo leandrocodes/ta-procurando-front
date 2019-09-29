3408123ad1134ea60877e997ce7cee02bbad5870
leandrocodes <leaxviana140@gmail.com>
2019-09-28 22:36:35 -0400

    border anim

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,19 +1,44 @@
 <template lang="pug">
   header.header
     b-nav(align='left')
+
       b-nav-item
         nuxt-link(to='/')
           img.header-logo(src='../assets/imgs/logomarca-taprocurando.svg')
+
     b-nav(align='right')
       div.nav-item
         b-nav-item Anúncie seu Imóvel
-        div.left-line
-        div.right-line
-        div.bottom-line
-        div.top-line
-      b-nav-item Viajante
-      b-nav-item Anunciante
-      b-nav-item Contato
+        animation-group
+          div.left-line
+          div.right-line
+          div.bottom-line
+          div.top-line
+
+      div.nav-item
+        b-nav-item Viajante
+        animation-group
+          div.left-line
+          div.right-line
+          div.bottom-line
+          div.top-line
+
+      div.nav-item
+        b-nav-item Anunciante
+        animation-group
+          div.left-line
+          div.right-line
+          div.bottom-line
+          div.top-line
+
+      div.nav-item
+        b-nav-item Contato
+        animation-group
+          div.left-line
+          div.right-line
+          div.bottom-line
+          div.top-line
+
 </template>
 <script>
 export default {
