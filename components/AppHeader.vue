b61530d6524b402f8f847fea4cb3d4ac5bcc34ee
leandrocodes <leaxviana140@gmail.com>
2019-09-28 22:05:22 -0400

    borders

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,19 +1,19 @@
-<template>
-  <header class="header">
-    <b-nav align="left">
-      <b-nav-item>
-        <nuxt-link to="/">
-          <img class="header-logo" src="../assets/imgs/logomarca-taprocurando.svg">
-        </nuxt-link>
-      </b-nav-item>
-    </b-nav>
-    <b-nav align="right">
-      <b-nav-item>Anúncie seu Imóvel</b-nav-item>
-      <b-nav-item>Viajante</b-nav-item>
-      <b-nav-item>Anunciante</b-nav-item>
-      <b-nav-item>Contato</b-nav-item>
-    </b-nav>
-  </header>
+<template lang="pug">
+  header.header
+    b-nav(align='left')
+      b-nav-item
+        nuxt-link(to='/')
+          img.header-logo(src='../assets/imgs/logomarca-taprocurando.svg')
+    b-nav(align='right')
+      div.nav-item
+        b-nav-item Anúncie seu Imóvel
+        div.left-line
+        div.right-line
+        div.bottom-line
+        div.top-line
+      b-nav-item Viajante
+      b-nav-item Anunciante
+      b-nav-item Contato
 </template>
 <script>
 export default {
