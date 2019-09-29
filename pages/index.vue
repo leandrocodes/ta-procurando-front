9117d3a320acc1508c1b575563f361c45c429079
leandrocodes <leaxviana140@gmail.com>
2019-09-29 13:17:30 -0400

    starting the search bar

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="index">
     <AppHeader />
+    <SearchBar />
   </div>
 </template>
 
 <script>
 import AppHeader from '../components/AppHeader'
+import SearchBar from '../components/SearchBar'
 export default {
   components: {
-    AppHeader
+    AppHeader,
+    SearchBar
   }
 }
 </script>
