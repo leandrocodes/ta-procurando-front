6a06acd474cbd6a52ddffb9218234a2a6db5112b
leandrocodes <leaxviana140@gmail.com>
2019-10-04 18:48:49 -0400

    undo

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,14 @@
 <template>
   <div class="index">
     <AppHeader />
-    <AppNavbar />
   </div>
 </template>
 
 <script>
 import AppHeader from '../components/AppHeader'
-import AppNavbar from '../components/AppNavbar'
 export default {
   components: {
-    AppHeader,
-    AppNavbar
+    AppHeader
   }
 }
 </script>
