ae23c042e23aedde8a3f0ececc5f4531e34897a3
leandrocodes <leaxviana140@gmail.com>
2019-10-04 19:03:15 -0400

    header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,4 +1,82 @@
 <template lang="pug">
+  header.header
+    b-nav(align='left' @click="open = !open").menu
+      b-nav-item
+        nuxt-link(to='/')
+          .header-logo
+      b-nav-item.burguer
+        .line.line1
+        .line.line2
+        .line.line3
+
+    b-nav(align='right').nav-links
+      div.nav-item
+        b-nav-item Anúncie seu Imóvel
+        animation-group
+          div.left-line
+          div.right-line
+          div.bottom-line
+          div.top-line
+
+      div.nav-item
+        b-nav-item Viajante
+        animation-group
+          div.left-line
+          div.right-line
+          div.bottom-line
+          div.top-line
+
+      div.nav-item
+        b-nav-item Anunciante
+        animation-group
+          div.left-line
+          div.right-line
+          div.bottom-line
+          div.top-line
+
+      div.nav-item
+        nuxt-link(to='/contato')
+          b-nav-item Contato
+          animation-group
+            div.left-line
+            div.right-line
+            div.bottom-line
+            div.top-line
+
+    b-nav.nav-mobile(v-if="open")
+      div.nav-item
+        b-nav-item Viajante
+        animation-group
+          div.left-line
+          div.right-line
+          div.bottom-line
+          div.top-line
+
+      div.nav-item
+        b-nav-item Anunciante
+        animation-group
+          div.left-line
+          div.right-line
+          div.bottom-line
+          div.top-line
+
+      div.nav-item
+        b-nav-item Anúnciar
+        animation-group
+          div.left-line
+          div.right-line
+          div.bottom-line
+          div.top-line
+
+      div.nav-item
+        nuxt-link(to='/contato')
+          b-nav-item Contato
+          animation-group
+            div.left-line
+            div.right-line
+            div.bottom-line
+            div.top-line
+
 </template>
 <script>
 export default {
