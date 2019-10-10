5ade02e30fa15ea7e3027389702afa556e65b3a2
leandrocodes <leaxviana140@gmail.com>
2019-10-10 18:01:03 -0400

    tabs stylish

--- pages/index.vue
+++ pages/index.vue
@@ -2,14 +2,22 @@
   .index
     AppHeader
     section.busca
-      .tabs
-        .tab.casa
+      div.tabs
+        div.tab.casa
           img(src="../assets/imgs/icon-casa.svg")
           | Casa
-        .tab.pousada Pousada
-        .tab.hotel Hotel
-        .tab.beira-mar Beira-mar
-        .tab.sitio Sitio
+        div.tab.pousada
+          img(src="../assets/imgs/icon-pousada.svg")
+          | Pousada
+        div.tab.hotel
+          img(src="../assets/imgs/icon-hotel.svg")
+          | Hotel
+        div.tab.beira-mar
+          img(src="../assets/imgs/icon-beiramar.svg")
+          | Beira-mar
+        div.tab.sitio
+          img(src="../assets/imgs/icon-sitio.svg")
+          |Sitio
 </template>
 
 <script>
